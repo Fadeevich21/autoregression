78d2896b83458b271e85fa4cecbb2e697e53dc76
Fadeevich21 <fad.danila053@gmail.com>
2023-02-19 22:23:48 +0300

    add class "autoregression_solve" and refactoring code

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from src.multiple_regression import MultipleRegression
-from src.sequence_generator import xi
-from src.autoregression import autoregression
+from src.autoregression_solve import AutoregressionSolve
 
-
-number_variant = 3
+variant = 3
 number_known_values = 2
 number_of_predictions = 10
+regression = MultipleRegression
 
 if __name__ == "__main__":
-    regression = MultipleRegression()
-    generator = xi(number_variant, number_known_values, number_of_predictions)
+    solve = AutoregressionSolve()
+    solve.set_variant(variant)
+    solve.set_number_known_values(number_known_values)
+    solve.set_number_of_predictions(number_of_predictions)
+    solve.set_regression(MultipleRegression)
 
-    sequence, predicted = next(generator)
-    x, y, alpha, beta = autoregression(regression, sequence, predicted, number_known_values, number_of_predictions)
-
-    t = [regression.predict(alpha, beta, x_i) for x_i in x]
-    r = regression.r_squared(alpha, beta, x, y)
+    quality_of_autoregression = solve.execute()
+    print(f"quality_of_autoregression: {quality_of_autoregression}")
---
+++ src/autoregression_solve.py
@@ -0,0 +1,42 @@
+from src.sequence_generator import xi
+from src.autoregression import autoregression
+from src.regression import Regression
+
+
+class AutoregressionSolve:
+
+    __variant: int = None
+    __number_known_values: int = None
+    __number_of_predictions: int = None
+    __regression: Regression = None
+
+    def __init__(self) -> None:
+        pass
+
+    def set_variant(self, variant) -> None:
+        self.__variant = variant
+
+    def set_number_known_values(self, number_known_values) -> None:
+        self.__number_known_values = number_known_values
+
+    def set_number_of_predictions(self, number_of_predictions) -> None:
+        self.__number_of_predictions = number_of_predictions
+
+    def set_regression(self, regression) -> None:
+        self.__regression = regression()
+
+    def __get_generator(self):
+        return xi(self.__variant, self.__number_known_values, self.__number_of_predictions)
+
+    def __autoregression(self, sequence, predicted):
+        return autoregression(self.__regression, sequence, predicted, self.__number_known_values,
+                              self.__number_of_predictions)
+
+    def execute(self) -> int | float:
+        generator = self.__get_generator()
+        sequence, predicted = next(generator)
+
+        x, y, alpha, beta = self.__autoregression(sequence, predicted)
+        quality_of_autoregression = self.__regression.r_squared(alpha, beta, x, y)
+
+        return quality_of_autoregression
